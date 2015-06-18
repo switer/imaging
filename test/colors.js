a6ceeec8f9dedc961de2111fb9ad487c06bca698
switer <guankaishe@gmail.com>
2015-06-18 23:30:37 +0800

    bin code clean up, for maintainable and readable

--- test/colors.js
+++ test/colors.js
@@ -1,9 +1,8 @@
-/*
-    ▁▂▃▄▅▆▇█▉▊▋▌▍▎▏■
-*/
+'use strict';
+
 var color = require('colors')
 var fs = require('fs')
-var pix = '▇'
+var pixels = repeat('▇', 39)
 
 function repeat (str, times) {
     var index = 0,
@@ -16,24 +15,24 @@ function repeat (str, times) {
 }
 
 /* 0,0,128   0,128,0 */
-console.log(repeat(pix, 39)['blue'] + '\n' + repeat(pix, 39).green);
+console.log(pixels.blue + '\n' + pixels.green);
 /* 0,0,0  128,128,0 */
-console.log(repeat(pix, 39).black + '\n' + repeat(pix, 39).yellow);
+console.log(pixels.black + '\n' + pixels.yellow);
 /* 0,128,128   128,0,0*/
-console.log(repeat(pix, 39).cyan + '\n' + repeat(pix, 39).red);
+console.log(pixels.cyan + '\n' + pixels.red);
 /* 128,0,128  192,192,192*/
-console.log(repeat(pix, 39).magenta + '\n' + repeat(pix, 39).white);
+console.log(pixels.magenta + '\n' + pixels.white);
 /* 128,128,128  192,192,192*/
-console.log(repeat(pix, 39).grey + '\n' + repeat(pix, 39).zebra);
+console.log(pixels.grey + '\n' + pixels.zebra);
 /* 0,0,255   0,255,0 */
-console.log(repeat(pix, 39)['blue'].grey + '\n' + repeat(pix, 39).green.grey);
+console.log(pixels.blue.grey + '\n' + pixels.green.grey);
 /* 128,128,128  255,255,0 */
-console.log(repeat(pix, 39).black.grey + '\n' + repeat(pix, 39).yellow.grey);
+console.log(pixels.black.grey + '\n' + pixels.yellow.grey);
 /* 0,255,255   255,0,0*/
-console.log(repeat(pix, 39).cyan.grey + '\n' + repeat(pix, 39).red.grey);
+console.log(pixels.cyan.grey + '\n' + pixels.red.grey);
 /* 255,0,255  255,255,255*/
-console.log(repeat(pix, 39).magenta.grey + '\n' + repeat(pix, 39).white.grey);
+console.log(pixels.magenta.grey + '\n' + pixels.white.grey);
 /* 128,128,128  128,128,128*/
-console.log(repeat(pix, 39).grey.grey + '\n' + repeat(pix, 39).zebra.grey);
+console.log(pixels.grey.grey + '\n' + pixels.zebra.grey);
 /* 0,0,255   0,255,0 */
-console.log(repeat(pix, 39).blue.cyan);
+console.log(pixels.blue.cyan);
--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var path = require('path')
 var imaging = require('../index.js');
-imaging.draw('../res/i.png', {width: 70}, function (resp, status) {
+
+imaging.draw(path.join(__dirname, '../res/i.png'), {width: 70}, function (resp, status) {
     console.log(status);
     console.log(resp);
 });
