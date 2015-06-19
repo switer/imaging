12e785968250e7748fc2f42cd3b21cdb05e71ece
switer <guankaishe@gmail.com>
2015-06-19 11:19:11 +0800

    runner file clean up support random char map

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 var path = require('path')
 var imaging = require('../index.js');
 
-imaging.draw(path.join(__dirname, '../res/i.png'), {width: 70}, function (resp, status) {
+imaging.draw(path.join(__dirname, '../res/i.png'), {width: 70, char: 'random'}, function (resp, status) {
     console.log(status);
     console.log(resp);
 });
