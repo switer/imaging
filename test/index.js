d1489077a98321d4b192d412fdff4ff9163b1e93
switer <guankaishe@gmail.com>
2015-11-20 11:08:02 +0800

    1. support piping; 2. run phantomjs without https error; 3. test case.

--- test/index.js
+++ test/index.js
@@ -2,8 +2,23 @@
 
 var path = require('path')
 var imaging = require('../index.js');
+var assert = require('assert')
 
-imaging.draw(path.join(__dirname, '../res/i.png'), {width: 70, char: 'charmap'}, function (resp, status) {
-    console.log(status);
-    console.log(resp);
-});
+describe('#API Methods', function () {
+    it('Load local file', function (done) {
+        this.timeout(5000)
+        imaging.draw(path.join(__dirname, '../res/i.png'), {left: 10}, function (resp, status) {
+            assert.equal(status, 'success')
+            console.log(resp)
+            done()
+        });
+    })
+    it('Load remote resource', function (done) {
+        this.timeout(5000)
+        imaging.draw('https://www.google.com.hk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', {width: 40, left: 10}, function (resp, status) {
+            assert.equal(status, 'success')
+            console.log(resp)
+            done()
+        });
+    })
+})
